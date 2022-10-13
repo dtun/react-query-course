93e5d1ad57e6c9d8fc632d5539914f8a965a089d
Danny Tunney <dtun@me.com>
2022-10-13 09:18:05 -0700

    Add label filtering

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -1,15 +1,28 @@
-import IssuesList from "../components/IssuesList";
-import LabelList from "../components/LabelList";
+import { useState } from 'react';
+import IssuesList from '../components/IssuesList';
+import LabelList from '../components/LabelList';
+
 export default function Issues() {
+  const [labels, setLabels] = useState([]);
+
   return (
     <div>
       <main>
         <section>
           <h1>Issues</h1>
-          <IssuesList />
+          <IssuesList labels={labels} />
         </section>
         <aside>
-          <LabelList />
+          <LabelList
+            selected={labels}
+            toggle={(newLabel) =>
+              setLabels((labelsState) =>
+                labelsState.includes(newLabel)
+                  ? labelsState.filter((label) => label !== newLabel)
+                  : labelsState.concat(newLabel)
+              )
+            }
+          />
         </aside>
       </main>
     </div>
