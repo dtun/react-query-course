4bc1f4a6def3d7d06cd7645bcc310d074f5ea91e
Danny Tunney <dtun@me.com>
2022-11-04 08:18:20 -0700

    Pass signal to queries for cancelation

--- src/components/IssueDetails.jsx
+++ src/components/IssueDetails.jsx
@@ -5,14 +5,14 @@ import { Comment } from './Comment';
 import { IssueHeader } from './IssueHeader';
 
 function useIssueData(issueNumber) {
-  return useQuery(['issues', issueNumber], () => {
-    return fetchWithError(`/api/issues/${issueNumber}`);
+  return useQuery(['issues', issueNumber], ({ signal }) => {
+    return fetchWithError(`/api/issues/${issueNumber}`, { signal });
   });
 }
 
 function useIssueComments(issueNumber) {
-  return useQuery(['issues', issueNumber, 'comments'], () => {
-    return fetchWithError(`/api/issues/${issueNumber}/comments`);
+  return useQuery(['issues', issueNumber, 'comments'], ({ signal }) => {
+    return fetchWithError(`/api/issues/${issueNumber}/comments`, { signal });
   });
 }
 
--- src/helpers/useUserData.ts
+++ src/helpers/useUserData.ts
@@ -4,7 +4,7 @@ import { fetchWithError } from './fetchWithError';
 export function useUserData(userId) {
   const usersData = useQuery(
     ['users', userId],
-    () => fetchWithError(`/api/users/${userId}`),
+    ({ signal }) => fetchWithError(`/api/users/${userId}`, { signal }),
     { enabled: Boolean(userId), staleTime: 1000 * 60 * 5 }
   );
 
