1dfc455f1aa646d979b6591adb77d879a728004a
Danny Tunney <dtun@me.com>
2022-12-20 09:05:31 -0700

    Return fetch

--- src/components/IssueAssignment.jsx
+++ src/components/IssueAssignment.jsx
@@ -14,7 +14,7 @@ export function IssueAssignment({ assignee, issueNumber }) {
   const queryClient = useQueryClient();
   const setAssignment = useMutation(
     (assignee) => {
-      fetch(`/api/issues/${issueNumber}`, {
+      return fetch(`/api/issues/${issueNumber}`, {
         method: 'PUT',
         headers: { 'content-type': 'application/json' },
         body: JSON.stringify({ assignee }),
